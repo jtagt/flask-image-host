06cc0b17a0e53e84cae8fb799daa59041522bc6b
Jackson <35349831+jtagt@users.noreply.github.com>
2018-09-27 15:43:07 -0700

    Update app.py

--- app.py
+++ app.py
@@ -42,18 +42,5 @@ def sharex():
 def get_file(name):
       return send_file(os.path.join("images/", name))
 
-@app.route('/no-one-can-you')
-def home():
-    return render_template('index.html')
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
-
-
 if __name__ == '__main__':
    app.run(debug = False, threaded=True)
